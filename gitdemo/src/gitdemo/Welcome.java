e6790513d00964b075d9c403b0696a115db36f96
Vaishnavii2003 <138214916+Vaishnavii2003@users.noreply.github.com>
2025-09-04 12:16:17 +0530

    Update Welcome.java

--- gitdemo/src/gitdemo/Welcome.java
+++ gitdemo/src/gitdemo/Welcome.java
@@ -4,6 +4,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git");
 		System.out.println("Pushing the code to github");
+		System.out.println("Pulling the code from github");
 	}
 
 }
